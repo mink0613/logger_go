8163457b4b5d7f3ca4c52f941c83cea3009dc04a
saea <saea@localhost.localdomain>
2019-02-07 17:31:55 +0900

    Check last log time for day over

--- logger.go
+++ logger.go
@@ -17,26 +17,35 @@ var printConsole bool
 var fpLog *os.File
 var err error
 var isOpen bool
+var lastLogDate string
 
 // SetFileName will set the log file name
 // and re initialize the logger
 func SetFileName(newFileName string) {
-
 	fileName = newFileName
 	initialized = false // in order to stop pollingData()
 	initialized = initialize()
 }
 
 func getNow() string {
-
 	return time.Now().Local().Format("2006-01-02")
 }
 
 // Log will write log string both on console and in file
 func Log(v ...string) {
 
+	logDate := getNow()
+
+	// If last log date is yesterday, then reset to today
+	if lastLogDate != "" && strings.Compare(lastLogDate, logDate) == -1 {
+
+		fileName = ""
+		initialized = false
+	}
+
 	if fileName == "" {
 		fileName = getNow() + "_log.txt"
+		lastLogDate = getNow()
 	}
 
 	if initialized == false {
@@ -53,23 +62,64 @@ func Log(v ...string) {
 func Debug(v ...string) {
 
 	v = append([]string{"***Debug***"}, v...)
-	Log(v...)
+
+	logDate := getNow()
+
+	// If last log date is yesterday, then reset to today
+	if lastLogDate != "" && strings.Compare(lastLogDate, logDate) == -1 {
+
+		fileName = ""
+		initialized = false
+	}
+
+	if fileName == "" {
+		fileName = getNow() + "_log.txt"
+		lastLogDate = getNow()
+	}
+
+	if initialized == false {
+		initialized = initialize()
+	}
+
+	functionName := getCallingFunctionName()
+
+	logStr := functionName + "() --> " + strings.Join(v[:], ", ")
+	log.Println(logStr)
 }
 
 // Error will log parameters with Debug string in the beginning
 func Error(v ...string) {
 
 	v = append([]string{"***Error ***"}, v...)
-	Log(v...)
+
+	logDate := getNow()
+
+	// If last log date is yesterday, then reset to today
+	if lastLogDate != "" && strings.Compare(lastLogDate, logDate) == -1 {
+
+		fileName = ""
+		initialized = false
+	}
+
+	if fileName == "" {
+		fileName = getNow() + "_log.txt"
+		lastLogDate = getNow()
+	}
+
+	if initialized == false {
+		initialized = initialize()
+	}
+
+	functionName := getCallingFunctionName()
+
+	logStr := functionName + "() --> " + strings.Join(v[:], ", ")
+	log.Println(logStr)
 }
 
 func getCallingFunctionName() string {
 
 	fpcs := make([]uintptr, 1)
-	// This is the value to skip the number of calling function names.
-	// Change this value as you wish.
-	skip := 3
-	runtime.Callers(skip, fpcs)
+	runtime.Callers(3, fpcs)
 
 	// get the info of the actual function that's in the pointer
 	return runtime.FuncForPC(fpcs[0] - 1).Name()
@@ -107,3 +157,4 @@ func initialize() bool {
 
 	return true
 }
+
